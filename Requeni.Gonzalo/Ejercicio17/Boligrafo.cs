d76bc5d5db99abfb7eef7b809a2764d32b724e86
gonzalorqn <36246342+gonzalorqn@users.noreply.github.com>
2019-03-26 12:01:46 -0300

    Add files via upload

---
+++ Requeni.Gonzalo/Ejercicio17/Boligrafo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_17
+{
+    public class Boligrafo
+    {
+        public const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(short tinta, ConsoleColor color)
+        {
+            this.tinta = tinta;
+            this.color = color;
+        }
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+
+        private void SetTinta(short tinta)
+        {
+            this.tinta += tinta;
+            if(this.tinta < 0)
+            {
+                this.tinta = 0;
+            }
+            else if(this.tinta > cantidadTintaMaxima)
+            {
+                this.tinta = cantidadTintaMaxima;
+            }
+        }
+
+        public void Recargar()
+        {
+            this.SetTinta(cantidadTintaMaxima);
+        }
+
+        public bool Pintar(int gasto, out string dibujo)
+        {
+            bool retorno = false;
+            dibujo = "";
+            for(int i=0;i<gasto;i++)
+            {
+                if(this.tinta > 0)
+                {
+                    dibujo += "*";
+                }
+            }
+            if (gasto <= this.tinta)
+                retorno = true;
+
+            this.SetTinta((short)-gasto);
+            
+            return retorno;
+        }
+    }
+}
---
+++ Requeni.Gonzalo/Ejercicio17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio_17;
+using Librerias;
+
+namespace Ejercicio17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boligrafo azul = new Boligrafo(100, ConsoleColor.Blue);
+            Boligrafo rojo = new Boligrafo(50, ConsoleColor.Red);
+            
+            //Console.WriteLine("Ingrese tinta azul a gastar: ");
+            //int tinta = Console.ReadLine();
+            Console.WriteLine(Test.Saludar());
+            Console.ReadLine();
+        }
+    }
+}
